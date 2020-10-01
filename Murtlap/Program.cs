82f59e924b2e38b73461661f8254b406eb473864
Ilya Zharov <nusii86@gmail.com>
2020-10-01 13:46:57 +0300

    translate v1

--- Murtlap/Program.cs
+++ Murtlap/Program.cs
@@ -7,6 +7,8 @@ using FB2Library;
 using System.IO;
 using System.Linq;
 using MenuMasterLib;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 
 namespace Murtlap
@@ -16,8 +18,12 @@ namespace Murtlap
         static void Main(string[] args)
         {
             var Menu = new MenuMasterAction<CommonData>();
-            Menu.AddItem("Make frequency dictionary", UniReader.Read);
-            Menu.AddItem("Translate", Translator.Translate);
+            Menu.AddItem("Make frequency dictionary", UniReader.Make);
+            Menu.AddItem("Open frequency dictionary from file (.csv)", UniReader.ReadCsv);
+            Menu.AddItem("Open frequency dictionary from file (.json)", UniReader.ReadJson);
+            Menu.AddItem("Update context", UniReader.UpdateContext);
+            Menu.AddItem("Translate", TranslatorCover.Translate);
+            Menu.AddItem("Set part of speech flag", TranslatorCover.Translate);
             Menu.PrintAndWait(new CommonData());
 
             Console.ReadLine();
@@ -27,49 +33,197 @@ namespace Murtlap
 
     }
 
-    class Translator
+    class TranslatorCover
     {
         public static void Translate(CommonData commonData)
         {
+            commonData.yTranslator = new YTranslator();
+            List<WordItem> items= commonData.wordItemsHandler.wordItems;
+
+            foreach(var i in items)
+            {
+                i.valueTranslate=YTranslator.Translate(i.value);
+                i.contextTranslate = YTranslator.Translate(i.context);
+            }
+
+            YTranslator.Translate(items.First().value);
+
+            string output = JsonConvert.SerializeObject(new { data = items });
+            File.WriteAllText(@"C:\temp\withContextwithTranslate.json", output);
+
+
+
+
+
             Console.ReadLine();
         }
     }
 
 
 
+
+
+
     class CommonData
     {
-
+        public WordItemsHandler wordItemsHandler;
+        public YTranslator yTranslator;
     }
 
     class UniReader
     {
-        public static async void Read(CommonData commonData)
+        public static async void Make(CommonData commonData)
         {
 
             List<string> data = await FbReaderCover.ReadDataFromFile(@"C:\temp\1.fb2");
             Double fCount = 0;
-            WordItemsHendler wih = new WordItemsHendler();
+            commonData.wordItemsHandler = new WordItemsHandler();
             foreach (string line in data)
             {
                 fCount++;
-                wih.AddWordFromBookString(line);
+                commonData.wordItemsHandler.AddWordFromBookString(line);
 
                 if (fCount < 20 || fCount % 200 == 0)
-                    wih.UpdateStat();
+                    commonData.wordItemsHandler.UpdateStat();
 
                 if (fCount < 20 || fCount % 20 == 0)
                 {
                     Console.Clear();
                     //Console.WriteLine("lines: " + fCount);//4150
                     Console.WriteLine(Math.Round((fCount / 4150) * 100, 2) + "%");//4150
-                    wih.PrintStat();
+                    commonData.wordItemsHandler.PrintStat();
                 }
             }
 
             string rnd = Guid.NewGuid().ToString().Split('-')[0];
-            File.WriteAllLines(@"C:\temp\1outMM"+ rnd + ".csv", wih.GetCsvData().ToArray());
+            File.WriteAllLines(@"C:\temp\1outMM" + rnd + ".csv", commonData.wordItemsHandler.GetCsvData().ToArray());
             Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>");
+        }
+
+        internal static void ReadCsv(CommonData obj)
+        {
+            obj.wordItemsHandler = new WordItemsHandler(@"C:\temp\1outMM7583cd39.csv");
+            obj.wordItemsHandler.PrintStat();
+        }
+
+        internal static void ReadJson(CommonData obj)
+        {
+            obj.wordItemsHandler = new WordItemsHandler(@"C:\temp\withContext.json", FileType.Json);
+            obj.wordItemsHandler.PrintStat();
+        }
+
+        internal static async void UpdateContext(CommonData commonData)
+        {
+            var mostPopular = commonData.wordItemsHandler.GetFirst(600);//.OrderBy(i => i.count).Take(50);
+            Dictionary<int, string> data = FbReaderCover.ReadDataFromFile(@"C:\temp\1.fb2").Result.Select((element, index) => new { element, index }).ToDictionary(ele => ele.index, ele => ele.element);
+
+
+            foreach (var item in mostPopular)
+            {
+                item.context = GetContext(data, item.value);
+            }
+
+
+
+            string output = JsonConvert.SerializeObject(new { data = mostPopular });
+            File.WriteAllText(@"C:\temp\withContext.json", output);
+
+            var t = 0;
+
+        }
+
+        private static string GetContext(Dictionary<int, string> data, string item)
+        {
+            //'!','?','.',',',';','-','—','…','«','»' { '.', '!', '?', '…', '«', '-', '—' }; '»',
+            List<string> contextCandidate = new List<string>();
+            List<char> startContextMarker = new List<char> { '.', '!', '?', '…', '—', ':', ';' };
+            List<char> endContextMarker = new List<char> { '.', '!', '?', '…', '—', ':', ';' };
+            //var lowItem = item.ToLower();
+
+            foreach (var line in data)
+            {
+                if (line.Value.Contains("Somiglio forse al giovane esaltato vagabondo che sarà giustiziato oggi"))
+                {
+                    var yyyyy = 0;
+                }
+                //var splited = line.Value.Split(' ');
+                item = " " + item.Trim() + " ";
+                if (line.Value.Contains(item))
+                {
+                    var index = line.Value.IndexOf(item);
+                    var startContextCandidate = line.Value.Substring(0, index);
+                    var endContextCandidate = line.Value.Substring(index + item.Length, line.Value.Length - index - item.Length);
+                    string temp = item;
+
+                    for (int i = startContextCandidate.Length - 1; i >= 0; i--)
+                    {
+                        char c = startContextCandidate[i];
+                        if (!startContextMarker.Contains(c))
+                        {
+                            temp = c.ToString() + temp;
+                        }
+                        else
+                        {
+                            if (c == '—')
+                            { temp = c.ToString() + temp; }
+                            break;
+                        }
+                    }
+
+                    // temp = temp + " " + item + " ";
+
+                    for (int i = 0; i < endContextCandidate.Length; i++)
+                    {
+                        char c = endContextCandidate[i];
+
+                        if (!endContextMarker.Contains(c))
+                        {
+                            temp = temp + c.ToString();
+
+                        }
+                        else
+                        {
+                            if (c != '—')
+                            {
+                                temp = temp + c.ToString();
+                            }
+                            break;
+                        }
+                    }
+
+                    if (temp.Contains("di Kiriat."))
+                    {
+                        var yyyyy = 0;
+                    }
+
+                    contextCandidate.Add(temp.Trim().Trim(',').Trim('«').Trim('»'));
+                }
+            }
+
+
+            string result = "";
+
+            if (contextCandidate.Count > 0)
+            {
+                if (contextCandidate.Count !=0)
+                    result= contextCandidate.OrderBy(c => c.Length).First();
+
+
+                if (contextCandidate.Count >= 5)
+                {
+                    var tempResult= contextCandidate.OrderBy(c => c.Length).Skip(4).First();
+
+                    if(tempResult.Length<200)
+                    {
+                        result=tempResult;
+                    }
+                    
+                }
+                    
+
+            }
+
+            return result;
         }
     }
 
@@ -78,11 +232,10 @@ namespace Murtlap
         public static async Task<List<string>> ReadDataFromFile(string path)
         {
             List<string> data = new List<string>();
-            var r = new FB2Reader();
-           // File.OpenRead();
-            var ttt = await r.ReadAsync(File.OpenRead(path), new XmlLoadSettings(new System.Xml.XmlReaderSettings()));  
+            FB2Reader reader = new FB2Reader();
+            FB2File file = await reader.ReadAsync(File.OpenRead(path), new XmlLoadSettings(new System.Xml.XmlReaderSettings()));
 
-            foreach (var i in ttt.MainBody.Sections)
+            foreach (var i in file.MainBody.Sections)
             {
                 foreach (var ic in i.Content)
                 {
@@ -93,17 +246,58 @@ namespace Murtlap
         }
     }
 
-    class WordItemsHendler
+    enum FileType
     {
-        private List<WordItem> wordItems;
+        Csv,
+        Json
+    }
+
+
+    class WordItemsHandler
+    {
+        public List<WordItem> wordItems { get; set; }
 
         private List<string> stat;
 
-        public WordItemsHendler()
+        public WordItemsHandler()
         {
             wordItems = new List<WordItem>();
             stat = new List<string>();
         }
+
+        public WordItemsHandler(string path, FileType type = FileType.Csv) : this()
+        {
+            if (type == FileType.Csv)
+            {
+                var fileData = File.ReadLines(path);
+                foreach (var line in fileData)
+                {
+                    var linesParts = line.Split(';');
+                    wordItems.Add(new WordItem()
+                    {
+                        value = linesParts[0],
+                        count = Int32.Parse(linesParts[1])
+                    });
+
+                }
+            }
+
+            if (type == FileType.Json)
+            {
+                var fileData = File.ReadAllText(path);
+                var tttt = JObject.Parse(fileData);
+                JArray hj = (JArray)tttt["data"];
+
+                foreach (var i in hj)
+                {
+                    wordItems.Add(JsonConvert.DeserializeObject<WordItem>(i.ToString()));
+                }
+            }
+
+
+            UpdateStat();
+        }
+
 
         public List<string> GetCsvData()
         {
@@ -115,31 +309,53 @@ namespace Murtlap
 
         public void AddWordFromBookString(string bookString)
         {
-            var candidates = bookString.Split(' ').Select(c => c.Trim().Trim(',').Trim('.').Trim('-')).Where(c=>!string.IsNullOrEmpty(c)).Where(c=>c.Length>=4);
+            var candidates = bookString.Split(' ').Select(c => MakeCleanWord(c)).Where(c => !string.IsNullOrEmpty(c)).Where(c => c.Length >= 2);
 
             var newCandidates = candidates.Where(c => !wordItems.Select(i => i.value).Contains(c));
             var seenCandidates = candidates.Where(c => !newCandidates.Contains(c));
             wordItems.AddRange(newCandidates.Select(c => new WordItem { value = c, count = 1 }));
 
-            //var seenWordItems = wordItems.Where(i => seenCandidates.Contains(i.value));
+
             foreach (var sc in seenCandidates)
             {
-                var v = wordItems.Where(i => sc==i.value).First();
+                var v = wordItems.Where(i => sc == i.value).First();
                 v.count = v.count + 1;
             }
-                                                  
+
+        }
+
+        private string MakeCleanWord(string s)
+        {
+            //c.Trim().Trim(',').Trim('.').Trim('-').Trim('—').Replace(';',' ').Trim(':').Trim('…')
+
+            List<char> badChars = new List<char>
+            {
+                '!','?','.',',',';','-','—','…','«','»'
+            };
+
+            foreach (var c in badChars)
+            {
+                if (s.Contains(c))
+                { s = s.Replace(c.ToString(), ""); }
+            }
+
+            //if (badChars.Any(i => s.Contains(i)))
+            //{
+            //    return "";
+            //}
+            return s;
         }
 
         public void UpdateStat()
         {
 
-            var topCount = wordItems.Select(i => i.count).Distinct().OrderByDescending(o=>o).Take(35);
-            var min =topCount.Count()>0? topCount.Min():0;         
+            var topCount = wordItems.Select(i => i.count).Distinct().OrderByDescending(o => o).Take(35);
+            var min = topCount.Count() > 0 ? topCount.Min() : 0;
             var ordered = wordItems.Where(i => i.count > min);
             ordered = ordered.Where(i => topCount.Contains(i.count)).OrderByDescending(i => i.count).Take(35);
 
             stat = new List<string>();
-            stat.Add("wordItems: "+wordItems.Count().ToString());
+            stat.Add("wordItems: " + wordItems.Count().ToString());
             stat.Add("__________________________________________");
             Console.WriteLine(wordItems.Count());
             stat.AddRange(ordered.Select(o => $"{o.value} {o.count}"));
@@ -153,6 +369,11 @@ namespace Murtlap
                 Console.WriteLine(i);
             }
         }
+
+        internal IEnumerable<WordItem> GetFirst(int v)
+        {
+            return wordItems.Take(v);
+        }
     }
 
     class WordItem
@@ -160,5 +381,11 @@ namespace Murtlap
         public string value;
 
         public int count;
+
+        public string context;
+
+        public string valueTranslate;
+
+        public string contextTranslate;
     }
 }
---
+++ Murtlap/YTranslator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Murtlap
+{
+    class YTranslator
+    {
+        string folderId = "";
+
+        string iamToken = "";
+
+        public static string Translate(string value)
+        {
+            try
+            {
+                var requestBody = new RecToYat(value);
+                string output = JsonConvert.SerializeObject(requestBody);
+
+                HttpClient client = new HttpClient();
+                //client.BaseAddress=new Uri(@"https://translate.api.cloud.yandex.net/translate/v2/translate");
+
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer 9f7L7euelZrPlp3I");
+                var content = new StringContent(output, Encoding.UTF8, "application/json");
+                var result = client.PostAsync(@"https://translate.api.cloud.yandex.net/translate/v2/translate", content).Result;
+
+                var res = result.Content.ReadAsStringAsync().Result;
+
+                var deserializedProduct = JsonConvert.DeserializeObject<ResFromYat>(res);
+
+
+
+
+
+                return deserializedProduct.translations.First().text;
+            }
+            catch (Exception)
+            {
+
+                return "";
+            }
+        }
+
+
+    }
+
+
+    class ResFromYat
+    {
+        public List<ResItem> translations;
+    }
+
+    class ResItem
+    {
+        public string text;
+    }
+
+    class RecToYat
+    {
+        public string sourceLanguageCode = "it";
+
+        public string targetLanguageCode = "ru";
+
+        public string format = "PLAIN_TEXT";
+
+        public List<string> texts;
+
+        public string folderId = "b1g4hkcf3kdmfid5n";
+
+
+        public RecToYat(string val)
+        {
+            texts = new List<string>();
+            texts.Add(val);
+        }
+    }
+}
